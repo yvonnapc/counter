392371103b084c27e0cece5809f376fdde45523d
yvonnapc <yvonnap.contreras@gmail.com>
2016-01-21 10:27:24 -0800

    Add initial files to project

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(event){
+  var countTo = $('#captain').val();
+  var countBy = $('#mate').val();
+  var pineapple = anyString.();
+
+
+  for (i = 0; i  > countTo ; i += countBy) {
+    $("#test").append();
+
+
+}
+
+event.preventDefault();
+});
